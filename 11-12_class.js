80dc32378550ddebc9735ab20fdb32c5c64fb001
Konstantin Shalimov <90028811+VladKsenobayt@users.noreply.github.com>
2021-11-27 09:59:03 +0300

    11-12 5 первых заданий

---
+++ 11-12_class.js
@@ -0,0 +1,58 @@
+console.log("zadanie 1");
+let city = new Object();
+var city1 = {
+    
+};
+city1["name"] = "ГородN";
+city1["population"]=10000000;
+console.log("zadanie 2");
+var city2 = {name:"ГородM", population:1e6};
+console.log("zadanie 3");
+function getName (cityname) {
+    return(cityname.name)
+}
+city2.getName=getName (cityname=city2) 
+city1.getName=getName (cityname=city1) 
+console.log(city1.getName)
+console.log(city2.getName)
+console.log("zadanie 4");
+function exportStr(cityname) {
+    var strok = "";
+    for (var k in cityname) {
+        if (cityname[k] != undefined) {
+            strok = strok+k+"="+cityname[k]+"\n";
+        }
+    }
+    return(strok)
+    //return("name="+cityname.name+"\npopulation="+cityname.population+"\n")
+}
+city2.exportStr=exportStr (cityname=city2) 
+city1.exportStr=exportStr (cityname=city1) 
+console.log(city1.exportStr)
+console.log(city2.exportStr)
+console.log("zadanie 5");
+function getObj() {    return(this)}
+function getCity(){
+    return(getObj())
+}
+//city1.getCity = getObj;
+//city2.getCity = getObj;
+//city2.getCity=getCity () 
+//getCity: getObj,
+//city1["getCity"] = getObj();
+//city2["getCity"] = getObj();
+//city1.getCity=getCity () 
+//function getObj () { return this }
+ 
+city1.getCity = getObj;
+city2.getCity = getObj;
+console.log(city1.getCity())
+console.log(city2.getCity())
+console.log("zadanie 6");
+
+console.log("zadanie 7");
+
+console.log("zadanie 8");
+
+console.log("zadanie 9");
+
